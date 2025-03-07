d0919a151dba9ef7f7eee887312063bb97251e1b
sydney <kimsydney18@gmail.com>
2025-03-07 10:38:21 -0500

    removed old app method from comments

--- src/main/java/com/sg/flooringmastery/App.java
+++ src/main/java/com/sg/flooringmastery/App.java
@@ -13,18 +13,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
 
-//        UserIO myIo = new UserIOImpl();
-//        // Instantiate the View and wire the UserIO implementation into it
-//        FlooringView myView = new FlooringView(myIo);
-//        // Instantiate the DAO
-//        FlooringDao myDao = new FlooringDaoImpl();
-//        // Instantiate the Service Layer and wire the DAO and Audit DAO into it
-//        FlooringService myService = new FlooringServiceImpl(myDao);
-//        // Instantiate the Controller and wire the Service Layer into it
-//        FlooringController controller = new FlooringController(myView, myService);
-//        // Kick off the Controller
-//        controller.run();
-
         AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
         appContext.scan("com.sg.flooringmastery");
         appContext.refresh();
